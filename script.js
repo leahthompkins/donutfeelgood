7a47b417d0e795be2a4180e92a638ab2a88a2e94
leahthompkins <36087700+leahthompkins@users.noreply.github.com>
2025-05-15 22:43:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-window.addEventListener('load', () => {
-  const emblaNode = document.querySelector('.embla');
-  const embla = EmblaCarousel(emblaNode, {
-    loop: true,
-    draggable: true,
-    align: 'center',
-    slidesToScroll: 1
+document.addEventListener('DOMContentLoaded', () => {
+  const splide = new Splide('#donut-carousel', {
+    type: 'loop',
+    perPage: 3,
+    perMove: 1,
+    gap: '1rem',
+    focus: 'center',
+    breakpoints: {
+      768: {
+        perPage: 2,
+      },
+      480: {
+        perPage: 1.5,
+      }
+    }
   });
+
+  splide.mount();
 
   document.querySelectorAll('.donut').forEach(button => {
     button.addEventListener('click', () => {
