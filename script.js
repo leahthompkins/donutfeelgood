831fb69c1214c6d1e65d3dd8e728259f4e5fa86a
leahthompkins <36087700+leahthompkins@users.noreply.github.com>
2025-05-15 22:10:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const emblaNode = document.querySelector('.embla');
-const embla = EmblaCarousel(emblaNode, {
-  loop: true,
-  draggable: true,
-  align: 'center',
-  slidesToScroll: 1
-});
+window.addEventListener('DOMContentLoaded', () => {
+  const emblaNode = document.querySelector('.embla');
+  const embla = EmblaCarousel(emblaNode, {
+    loop: true,
+    draggable: true,
+    align: 'center',
+    slidesToScroll: 1
+  });
 
-document.querySelectorAll('.donut').forEach(button => {
-  button.addEventListener('click', () => {
-    const mood = button.innerText;
-    alert(`You selected: ${mood}`);
+  document.querySelectorAll('.donut').forEach(button => {
+    button.addEventListener('click', () => {
+      const mood = button.innerText;
+      alert(`You selected: ${mood}`);
+    });
   });
 });
