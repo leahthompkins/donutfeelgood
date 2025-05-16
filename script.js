024bd32cbf2bca0d1eb4d491102e766a73e2dd30
leahthompkins <36087700+leahthompkins@users.noreply.github.com>
2025-05-15 21:56:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const donutButtons = document.querySelectorAll('.donut');
-donutButtons.forEach(button => {
+const emblaNode = document.querySelector('.embla');
+const embla = EmblaCarousel(emblaNode, {
+  loop: true,
+  draggable: true,
+  align: 'center',
+  slidesToScroll: 1
+});
+
+document.querySelectorAll('.donut').forEach(button => {
   button.addEventListener('click', () => {
     const mood = button.innerText;
     alert(`You selected: ${mood}`);
   });
-});
-
-const emblaNode = document.querySelector('.embla');
-const embla = EmblaCarousel(emblaNode, {
-  loop: true,
-  align: 'center',
-  draggable: true,
-  slidesToScroll: 1
 });
